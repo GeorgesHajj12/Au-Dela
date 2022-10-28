69f895b8a234a8af3f6e27ff4da34cb440f0643f
GeorgesHajj12 <88936025+GeorgesHajj12@users.noreply.github.com>
2022-10-28 21:38:24 +0300

    FAQ edit
    edited FAQ page

--- faq.php
+++ faq.php
@@ -9,9 +9,77 @@
   integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="header.css">
+ 
 </head>
 <body>
+<!--Section: FAQ-->
+<section>
+  <h3 class="text-center mb-4 pb-2  fw-bold">FAQ</h3>
+  <p class="text-center mb-5">
+    Find the answers for the most frequently asked questions below
+  </p>
 
+  <div class="row">
+    <div class="col-md-6 col-lg-4 mb-4">
+      <h6 class="mb-3 "><i class="far fa-paper-plane  pe-2"></i> A simple
+        question?</h6>
+      <p>
+        <strong><u>Absolutely!</u></strong> We work with top payment companies which guarantees
+        your
+        safety and
+        security. All billing information is stored on our payment processing partner.
+      </p>
+    </div>
+
+    <div class="col-md-6 col-lg-4 mb-4">
+      <h6 class="mb-3 "><i class="fas fa-pen-alt  pe-2"></i> A question
+        that
+        is longer then the previous one?</h6>
+      <p>
+        <strong><u>Yes, it is possible!</u></strong> You can cancel your subscription anytime in
+        your
+        account. Once the subscription is
+        cancelled, you will not be charged next month.
+      </p>
+    </div>
+
+    <div class="col-md-6 col-lg-4 mb-4">
+      <h6 class="mb-3 "><i class="fas fa-user  pe-2"></i> A simple
+        question?
+      </h6>
+      <p>
+        Currently, we only offer monthly subscription. You can upgrade or cancel your monthly
+        account at any time with no further obligation.
+      </p>
+    </div>
+
+    <div class="col-md-6 col-lg-4 mb-4">
+      <h6 class="mb-3 "><i class="fas fa-rocket  pe-2"></i> A simple
+        question?
+      </h6>
+      <p>
+        Yes. Go to the billing section of your dashboard and update your payment information.
+      </p>
+    </div>
+
+    <div class="col-md-6 col-lg-4 mb-4">
+      <h6 class="mb-3 "><i class="fas fa-home  pe-2"></i> A simple
+        question?
+      </h6>
+      <p><strong><u>Unfortunately no</u>.</strong> We do not issue full or partial refunds for any
+        reason.</p>
+    </div>
+
+    <div class="col-md-6 col-lg-4 mb-4">
+      <h6 class="mb-3 "><i class="fas fa-book-open  pe-2"></i> Another
+        question that is longer than usual</h6>
+      <p>
+        Of course! We’re happy to offer a free plan to anyone who wants to try our service.
+      </p>
+    </div>
+  </div>
+</section>
+<!--Section: FAQ-->
 </body>
 </html>
 <?php require_once('footer.php'); ?>   
--- jewlryEducation.php
+++ jewlryEducation.php
@@ -1,1 +1,2 @@
 <?php require_once('header.php'); ?>
+<?php require_once('footer.php'); ?>
